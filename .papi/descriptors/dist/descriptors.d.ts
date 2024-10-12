7e569234d215692b594e4cfbc3a7fbc689a513eb
71walceli <antonini44354@gmail.com>
2024-10-12 10:26:10 -0500

    Adding chain data

---
+++ .papi/descriptors/dist/descriptors.d.ts
@@ -0,0 +1,5 @@
+export declare const People_rococo: Record<string, object>;
+export declare const Polkadot: Record<string, object>;
+export declare const Westend: Record<string, object>;
+export declare const Paseo: Record<string, object>;
+export declare const Kusama: Record<string, object>;
