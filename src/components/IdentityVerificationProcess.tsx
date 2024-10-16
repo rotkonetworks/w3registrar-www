5fa907410700a2df3a0e05fd8c2568ec903fe267
71walceli <antonini44354@gmail.com>
2024-10-16 15:25:03 -0500

    Deleter unused code

--- src/components/IdentityVerificationProcess.tsx
+++ src/components/IdentityVerificationProcess.tsx
@@ -7,47 +7,6 @@ import CompletionPage from './CompletionPage';
 
 import { useAccounts } from "@reactive-dot/react";
 
-import {
-  useConnectedWallets,
-  useWallets,
-  useWalletConnector,
-  useWalletDisconnector,
-} from "@reactive-dot/react";
-
-export function Wallets() {
-  const wallets = useWallets();
-  const connectedWallets = useConnectedWallets();
-
-  const [_, connectWallet] = useWalletConnector();
-  const [__, disconnectWallet] = useWalletDisconnector();
-
-  return (
-    <section>
-      <header>
-        <h3>Wallet connection</h3>
-      </header>
-      <article>
-        <h4>Wallets</h4>
-        <ul>
-          {wallets.map((wallet) => (
-            <li key={wallet.id}>
-              <div>{wallet.name}</div>
-              <div>
-                {connectedWallets.includes(wallet) ? (
-                  <button onClick={() => disconnectWallet(wallet)}>
-                    Disconnect
-                  </button>
-                ) : (
-                  <button onClick={() => connectWallet(wallet)}>Connect</button>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </section>
-  );
-}
 
 export function Accounts() {
   const accounts = useAccounts();
