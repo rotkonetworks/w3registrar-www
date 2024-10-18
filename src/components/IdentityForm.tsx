c907ca837191e2a65a1c84eb6b316f5c7ee25a5f
71walceli <antonini44354@gmail.com>
2024-10-18 18:34:34 -0500

    Filtering by fields present in the form

--- src/components/IdentityForm.tsx
+++ src/components/IdentityForm.tsx
@@ -89,23 +89,24 @@ const IdentityForm: React.FC = () => {
   return (
     <div className="space-y-4">
       <h2 className="text-xl font-bold mb-4 text-stone-800">Identity</h2>
-      {Object.entries(_identity).map(([key, value]) => (
-        <div key={key} className="flex flex-col">
-          <label className="text-sm text-stone-600 mb-1">{fieldNames[key]}</label>
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => handleChange(key, e.target.value)}
-            placeholder={placeholders[key]}
-            className="border-b border-stone-400 px-0 py-2 text-sm text-stone-800 focus:outline-none focus:border-stone-600 placeholder-stone-400"
-            required={key === 'displayName'}
-          />
-          {errors[key] && <p className="text-red-700 text-sm">{typeof errors[key] === "boolean"
-            ? <>Invalid format for {fieldNames[key]}</>
-            : errors[key]
-          }</p>}
-        </div>
-      ))}
+      {IdentityFormFields.map((key) => {
+        const value = _identity[key] 
+        return (
+          <div key={key} className="flex flex-col">
+            <label className="text-sm text-stone-600 mb-1">{fieldNames[key]}</label>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => handleChange(key, e.target.value)}
+              placeholder={placeholders[key]}
+              className="border-b border-stone-400 px-0 py-2 text-sm text-stone-800 focus:outline-none focus:border-stone-600 placeholder-stone-400"
+              required={key === 'displayName'} />
+            {errors[key] && <p className="text-red-700 text-sm">{typeof errors[key] === "boolean"
+              ? <>Invalid format for {fieldNames[key]}</>
+              : errors[key]}</p>}
+          </div>
+        );
+      })}
       <button
         onClick={handleSubmitIdentity}
         className="mt-6 w-full bg-stone-700 hover:bg-stone-800 text-white py-2 text-sm font-semibold transition duration-300"
