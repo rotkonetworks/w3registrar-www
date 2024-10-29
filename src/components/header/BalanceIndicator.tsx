38b19ef99282bb70e2f968f37a8a4507fdf1a1cc
71walceli <antonini44354@gmail.com>
2024-10-28 21:15:18 -0500

    Added amount formatter

--- src/components/header/BalanceIndicator.tsx
+++ src/components/header/BalanceIndicator.tsx
@@ -6,6 +6,15 @@ import { appState } from "~/App"
 export const BalanceIndicator: React.FC = () => {
   const appStateSnap = useSnapshot(appState)
 
+  const formatValue = (amount: bigint | undefined): string => {
+    if (!amount) {
+      return "...";
+    }
+    amount = amount.toString()
+    amount = BigNumber(amount).dividedBy(BigNumber(10).pow(appStateSnap.chain.tokenDecimals));
+    return `${amount.toLocaleString()} ${appStateSnap.chain.tokenSymbol}`;
+  }
+
   useEffect(() => {
     import.meta.env.DEV && console.log({
       free: Number(appStateSnap.account.balance?.free) / 10 ** appStateSnap.chain.tokenDecimals,
