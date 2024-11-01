babbf380adcb9c574d45fa87aacb3edfe0f3b1b7
71walceli <antonini44354@gmail.com>
2024-10-31 23:58:59 -0500

    Swapping up app arount ContectProvider

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,22 @@ import { ChainProvider, ReactiveDotProvider } from '@reactive-dot/react'
 import { config } from './api/config'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ReactiveDotProvider config={config}>
-      <ChainProvider chainId={config[appState.chain.id]}>
-        <div className='dark:bg-black min-h-0px'>
-          <App />
-        </div>
-      </ChainProvider>
-    </ReactiveDotProvider>
-  </React.StrictMode>
-)
+const Main: React.FC = () => {
+  const appStateSnap = useSnapshot(appState)
+  
+  return <>
+    <React.StrictMode>
+      <ReactiveDotProvider config={config}>
+        {appStateSnap.chain.id &&
+          <ChainProvider chainId={appStateSnap.chain.id}>
+            <div className='dark:bg-black min-h-0px'>
+              <App />
+            </div>
+          </ChainProvider>
+        }
+      </ReactiveDotProvider>
+    </React.StrictMode>
+  </>
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Main />)
