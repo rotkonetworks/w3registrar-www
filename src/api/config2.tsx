a0ea0d23cba04da520c5156e645dde3129d23191
antonini44354 <antonini44354@gmail,com>
2024-11-14 17:21:35 -0500

    Moved config creation inode

--- src/api/config2.tsx
+++ src/api/config2.tsx
@@ -60,6 +60,92 @@ export const ConfigProvider = ({ children }) => {
     import.meta.env.DEV && console.log("Starting smoldot worker")
   }
 
+  function createConfig(): ApiConfig {
+    return {
+      chains: {
+        people_polkadot: {
+          name: "Polkadot",
+          descriptor: people_polkadot,
+          provider: getSmProvider(worker?.addChain({ chainSpec: peoplePolkadotChainSpec })),
+        },
+        people_kusama: {
+          name: "Kusama",
+          descriptor: people_kusama,
+          provider: getSmProvider(worker?.addChain({ chainSpec: peopleKusamaChainSpec })),
+        },
+        people_westend: {
+          name: "Westend",
+          descriptor: people_westend,
+          provider: getSmProvider(worker?.addChain({ chainSpec: peopleWestendChainSpec })),
+        },
+        polkadot: {
+          name: "Polkadot",
+          descriptor: polkadot,
+          provider: getSmProvider(worker?.addChain({ chainSpec: polkadotChainSpec })),
+        },
+        kusama: {
+          name: "Kusama",
+          descriptor: kusama,
+          provider: getSmProvider(worker?.addChain({ chainSpec: kusamaChainSpec })),
+        },
+        westend: {
+          name: "Westend",
+          descriptor: westend,
+          provider: getSmProvider(worker?.addChain({ chainSpec: westendChainSpec })),
+        },
+      },
+      wallets: [
+        new InjectedWalletAggregator(),
+        new LedgerWallet(),
+        new WalletConnect({
+          projectId: import.meta.env.VITE_APP_WALLET_CONNECT_PROJECT_ID,
+          providerOptions: {
+            metadata: {
+              name: "w3reg",
+              description: "web3 registrar.",
+              url: globalThis.origin,
+              icons: ["/logo.png"],
+            },
+          },
+          chainIds: [
+            "polkadot:67fa177a097bfa18f77ea95ab56e9bcd", // people-polkadot
+            "polkadot:1eb6fb0ba5187434de017a70cb84d4f4", // people-westend
+            "polkadot:c1af4cb4eb3918e5db15086c0cc5ec17", // people-kusama
+          ],
+          optionalChainIds: [
+            "polkadot:42a6fe2a73c2a8920a8ece6bdbaa63fc", // people-rococo
+            "polkadot:91b171bb158e2d3848fa23a9f1c25182", // polkadot
+            "polkadot:b0a8d493285c2df73290dfb7e61f870f", // kusama
+            "polkadot:e143f23803ac50e8f6f8e62695d1ce9e", // westend
+          ],
+        }),
+      ],
+    } as const satisfies ApiConfig;
+  }
+
+  function createConfigWithCustomEndpoint(chainId: string, endpoint: string): ApiConfig {
+    const newConfig = createConfig();
+
+    return {
+      ...newConfig,
+      chains: {
+        ...newConfig.chains,
+        people_rococo: {
+          name: "Rococo",
+          descriptor: people_rococo,
+          provider: () => getWsProvider(import.meta.env.VITE_APP_DEFAULT_WS_URL),
+          registrarIndex: import.meta.env.VITE_APP_REGISTRAR_INDEX__PEOPLE_KUSAMA,
+        },
+        rococo: {
+          name: "Rococo",
+          descriptor: rococo,
+          provider: () =>
+            getWsProvider(import.meta.env.VITE_APP_DEFAULT_WS_URL_RELAY),
+        },
+      },
+    };
+  }
+
   const validateUrl = (url: string): { isValid: boolean; message: string } => {
     if (!url.trim()) return { isValid: false, message: "URL cannot be empty" };
     try {
@@ -108,84 +194,3 @@ export const useConfig = () => {
   }
   return context;
 }
-
-export function createConfig(): ApiConfig {
-  return {
-    chains: {
-      people_polkadot: {
-        name: "Polkadot",
-        descriptor: people_polkadot,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: peoplePolkadotChainSpec })),
-      },
-      people_kusama: {
-        name: "Kusama",
-        descriptor: people_kusama,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: peopleKusamaChainSpec })),
-      },
-      people_westend: {
-        name: "Westend",
-        descriptor: people_westend,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: peopleWestendChainSpec })),
-      },
-      polkadot: {
-        name: "Polkadot",
-        descriptor: polkadot,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: polkadotChainSpec })),
-      },
-      kusama: {
-        name: "Kusama",
-        descriptor: kusama,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: kusamaChainSpec })),
-      },
-      westend: {
-        name: "Westend",
-        descriptor: westend,
-        provider: getSmProvider(configStore.worker.addChain({ chainSpec: westendChainSpec })),
-      },
-    },
-    wallets: [
-      new InjectedWalletAggregator(),
-      new LedgerWallet(),
-      new WalletConnect({
-        projectId: import.meta.env.VITE_APP_WALLET_CONNECT_PROJECT_ID,
-        providerOptions: {
-          metadata: {
-            name: "w3reg",
-            description: "web3 registrar.",
-            url: globalThis.origin,
-            icons: ["/logo.png"],
-          },
-        },
-        chainIds: [
-          "polkadot:67fa177a097bfa18f77ea95ab56e9bcd", // people-polkadot
-          "polkadot:1eb6fb0ba5187434de017a70cb84d4f4", // people-westend
-          "polkadot:c1af4cb4eb3918e5db15086c0cc5ec17", // people-kusama
-        ],
-        optionalChainIds: [
-          "polkadot:42a6fe2a73c2a8920a8ece6bdbaa63fc", // people-rococo
-          "polkadot:91b171bb158e2d3848fa23a9f1c25182", // polkadot
-          "polkadot:b0a8d493285c2df73290dfb7e61f870f", // kusama
-          "polkadot:e143f23803ac50e8f6f8e62695d1ce9e", // westend
-        ],
-      }),
-    ],
-  } as const satisfies ApiConfig;
-}
-
-export function createConfigWithCustomEndpoint(chainId: string, endpoint: string): ApiConfig {
-  const newConfig = createConfig();
-
-  return {
-    ...newConfig,
-    chains: {
-      ...newConfig.chains,
-      [chainId]: {
-        ...newConfig.chains[chainId],
-        name: "Custom WS",
-        descriptor: people_rococo,
-        provider: new WsProvider(endpoint),
-      },
-    },
-  };
-}
-
