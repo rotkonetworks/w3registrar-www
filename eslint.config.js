4ca2f758a40e0f90dbe0dce538ec0eb250985d0a
71walceli <antonini44354@gmail.com>
2024-11-16 09:32:45 -0500

    Refined ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,40 +3,35 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    ignores: [
-      "**/node_modules/**",
-      "**/dist/**",
-      ".papi/*",
-      ".papi/**",
-      "**/.papi/**",
-      "**/.papi/*",
-      "**/.papi/",
-      "**/.papi",
-      "*/.papi/**",
-      "*/.papi/*",
-      "*/.papi/",
-      "*/.papi",
-      "/.papi/**",
-      "/.papi/*",
-      "/.papi/",
-      "/.papi",
-      ".papi/**",
-      ".papi/*",
-      ".papi/",
-      ".papi",
-      ".papi",
-      "/.papi",
-    ],
-  },
-  {
-    languageOptions: { globals: globals.browser }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { 
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        },
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "react": pluginReact
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      //...pluginReact.configs.recommended.rules
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ];
