b45b75cac37e362285b387f50a4eb3e1a3c2ad2f
antonini44354 <antonini44354@gmail,com>
2024-11-18 16:35:53 -0500

    Added app store

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,3 +14,20 @@ interface ChainInfo {
 export const chainStore: ChainInfo = proxy({
   id: import.meta.env.VITE_APP_DEFAULT_CHAIN || Object.keys(config.chains)[0],
 })
+
+type AppStore = {
+  isDarkMode: boolean;
+  isReady: boolean;
+  isDev: boolean;
+  isProd: boolean;
+  isTest: boolean;
+  isDevTest: boolean;
+};
+export const appStore = proxy<AppStore>({
+  isDarkMode: true,
+  isReady: false,
+  isDev: import.meta.env.DEV,
+  isProd: import.meta.env.PROD,
+  isTest: import.meta.env.MODE === 'test',
+  isDevTest: import.meta.env.MODE === 'development',
+})
