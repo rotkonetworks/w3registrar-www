bf1273638129dbfcd1a187c8a36cb75aed42bb69
antonini44354 <antonini44354@gmail,com>
2024-11-19 16:09:07 -0500

    Revuewed build script for building on workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,8 +21,14 @@ done
 export PATH="/usr/local/bin:/usr/bin:/bin"
 
 readonly REQUIRED_VARS=(
-    "VITE_APP_WALLET_CONNECT_PROJECT_ID"
-    "VITE_APP_DEFAULT_WS_URL"
+    VITE_APP_WALLET_CONNECT_PROJECT_ID
+    VITE_APP_DEFAULT_WS_URL
+    VITE_APP_DEFAULT_WS_URL_RELAY
+    VITE_APP_REGISTRAR_INDEX__PEOPLE_POLKADOT
+    VITE_APP_REGISTRAR_INDEX__PEOPLE_KUSAMA
+    VITE_APP_REGISTRAR_INDEX__PEOPLE_WESTEND
+    VITE_APP_REGISTRAR_INDEX__PEOPLE_ROCOCO
+    VITE_APP_CHALLENGES_API_URL
 )
 
 for var in "${REQUIRED_VARS[@]}"; do
@@ -32,15 +38,36 @@ for var in "${REQUIRED_VARS[@]}"; do
     fi
 done
 
+cat << EOF > ./node_modules/css-tree/lib/data-patch.js
+import * as patch from "../data/patch.json"
+
+export default patch;
+EOF
+
+cat << EOF > ./node_modules/css-tree/lib/version.js
+export { version } from "../package.json";
+EOF
+
+# vite build will fail with 
+#   error during build:
+#   undefined
+# So this script is required to get actual error message:
+cat << EOF > ./build.js
+const vite = require('vite');
+
+vite.build();
+EOF
+RUN bun ./build.js
+
 if command -v bun &> /dev/null; then
     echo "Using Bun for build"
-    if ! bun run build; then
+    if ! bun ./index.js; then
         echo "Error: 'bun run build' failed." >&2
         exit 1
     fi
 elif command -v npm &> /dev/null; then
     echo "Using npm for build"
-    if ! npm run build; then
+    if ! npm ./index.js; then
         echo "Error: 'npm run build' failed." >&2
         exit 1
     fi
