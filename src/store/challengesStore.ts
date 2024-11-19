291fff3c47b9375240159772762f0238610ca7fe
antonini44354 <antonini44354@gmail,com>
2024-11-19 12:17:59 -0500

    Added challenges store

---
+++ src/store/challengesStore.ts
@@ -0,0 +1,29 @@
+import { proxy } from "valtio";
+
+export enum ChallengeStatus {
+  Failed = -2,
+  Unknown = -1,
+  Pending = 0,
+  Passed = 1,
+  Optional = 2,
+}
+
+export interface Challenge {
+  status: ChallengeStatus;
+  code: string;
+}
+
+export interface ChallengeStore {
+  discord?: Challenge;
+  display?: Challenge;
+  email?: Challenge;
+  legal?: Challenge;
+  github?: Challenge;
+  matrix?: Challenge;
+  twitter?: Challenge;
+  image?: Challenge;
+  pgpFingerprint?: Challenge;
+  web?: Challenge;
+}
+
+export const challengeStore = proxy<ChallengeStore>({})
