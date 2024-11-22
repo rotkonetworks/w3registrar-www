e739c1212d8598615c068a11677bac7e9468b8a0
antonini44354 <antonini44354@gmail,com>
2024-11-22 14:50:02 -0500

    Refactored Account store away to itw own files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
 import { Chains } from '@reactive-dot/core';
 import { proxy } from 'valtio'
-import { config } from '~/api/config';
 
 export * from './userSore'
 
@@ -12,23 +11,6 @@ interface ChainInfo {
 }
 
 export const chainStore: ChainInfo = proxy({
-  id: import.meta.env.VITE_APP_DEFAULT_CHAIN || Object.keys(config.chains)[0],
-})
-
-type AppStore = {
-  isDarkMode: boolean;
-  isReady: boolean;
-  isDev: boolean;
-  isProd: boolean;
-  isTest: boolean;
-  isDevTest: boolean;
-};
-export const appStore = proxy<AppStore>({
-  isDarkMode: true,
-  isReady: false,
-  isDev: import.meta.env.DEV,
-  isProd: import.meta.env.PROD,
-  isTest: import.meta.env.MODE === 'test',
-  isDevTest: import.meta.env.MODE === 'development',
+  id: localStorage.getItem('chain') as keyof Chains || import.meta.env.VITE_APP_DEFAULT_CHAIN,
 })
 
