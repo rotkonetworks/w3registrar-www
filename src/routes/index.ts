475704a9ef296e4b62bbd16f44671808064f09a6
71walceli <antonini44354@gmail.com>
2024-11-30 23:49:31 -0500

    Removed onnecessary input

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import type React from 'react'
 import { lazy } from 'react'
-import { IdentityRegistrarComponent } from '~/components/identity-registrar'
 import Home from '~/pages/home'
 
 const ErrorPage = lazy(() => import('~/pages/error-page'))
