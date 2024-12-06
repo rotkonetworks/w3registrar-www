e6e05484cdb570edd28028d317caa1d8d3afef3b
71walceli <antonini44354@gmail.com>
2024-12-06 01:28:28 -0500

    Made loader dark if app was dark

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useSnapshot } from "valtio";
+import { appStore } from "~/store/AppStore";
+
 export function Loading() {
+  const { isDarkMode } = useSnapshot(appStore)
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkMode)
+  }, [isDarkMode])
+  
   return (
-    <div className='h-100vh flex-center'>
+    <div className={'h-100vh flex-center ' + isDarkMode ? 'bg-[#2C2B2B] text-[#FFFFFF]' : 'bg-[#FFFFFF] text-[#1E1E1E]'}>
       Loading...
     </div>
   );
