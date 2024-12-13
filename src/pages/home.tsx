f50aefd8db5e718c3359e7db8aa9badf455b1c55
71walceli <antonini44354@gmail.com>
2024-12-13 01:02:50 -0500

    Refactored all reactiveDOT vars into own context

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,8 @@
-import { ChainProvider, ReactiveDotProvider } from '@reactive-dot/react';
-import { useProxy } from 'valtio/utils';
 import { IdentityRegistrarComponent } from '~/components/identity-registrar';
 import { chainStore as _chainStore } from '~/store/ChainStore';
-import { config } from '~/api/config';
-import { Suspense, useDeferredValue } from 'react';
-import { Loading } from './Loading';
 
 function Home() {
-  const chainId = useProxy(_chainStore).id
-  
-  return (
-    <ReactiveDotProvider config={config}>
-      <ChainProvider chainId={chainId}>
-        <Suspense fallback={<Loading />}>
-          <IdentityRegistrarComponent />
-        </Suspense>
-      </ChainProvider>
-    </ReactiveDotProvider>
-  );
+  return <IdentityRegistrarComponent />
 }
 
 export default Home;
