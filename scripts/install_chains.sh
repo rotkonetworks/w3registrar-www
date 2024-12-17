d399d845b55caf1924c4e6483e4ef564e9820d66
71walceli <antonini44354@gmail.com>
2024-12-17 11:28:31 -0500

    Added scripts to install parachains

---
+++ scripts/install_chains.sh
@@ -0,0 +1,12 @@
+networks=("polkadot" "westend" "ksmcc3")
+parachains=("asset_hub" "bridge_hub" "collectives" "encointer" "people")
+
+for network in "${networks[@]}"; do
+  for parachain in "${parachains[@]}"; do
+    echo "Processing $network $parachain"
+    network_id="${network}_${parachain}"
+    command="bunx papi add ${network_id} -n ${network_id}"
+    echo $command
+    $command
+  done
+done
