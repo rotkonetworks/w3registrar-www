fce7ac9281cb83144b477790fa87e70a4e184fa7
71walceli <71walceli@71walceli.dev>
2024-12-28 00:28:53 -0500

    Enabled Source maps, disabled minification on dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import unocss from 'unocss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    unocss()
-  ],
-  server: {
-    port: 3333
-  },
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src'),
-      '@': resolve(__dirname, 'src'),
-    }
-  },
-  build: {
-    sourcemap: "inline", // Enable source map generation
-    minify: true, // enable minification
-  },
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    plugins: [
+      react(),
+      unocss()
+    ],
+    server: {
+      port: 3333
+    },
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, 'src'),
+        '@': resolve(__dirname, 'src'),
+      }
+    },
+    build: {
+      sourcemap: isDevelopment, // Enable source map generation only in development
+      minify: !isDevelopment, // Enable minification only in production
+      mode: isDevelopment ? 'development' : 'production',
+    },
+  }
 })
