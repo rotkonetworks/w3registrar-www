0ff54a5487d07285951eafe74b8e6cb65e8cade0
71walceli <71walceli@71walceli.dev>
2024-12-28 00:46:00 -0500

    Enabled Source maps, disabled minification on dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 import unocss from 'unocss/vite'
 
 export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
+  const isDevelopment = ['development', 'preview'].includes(mode);
 
   return {
     plugins: [
