4f54ed6053cd36618e74585b6ce1f2fd135ebb09
71walceli <71walceli@71walceli.dev>
2025-01-15 16:16:38 -0500

    Added URL param and haldling for selected tab

--- src/components/identity-registrar.tsx
+++ src/components/identity-registrar.tsx
@@ -148,7 +148,7 @@ const MainContent = ({
   const [currentPage, setCurrentPage] = useState(0)
     
   useEffect(() => {
-    if (!urlParams. ) {
+    if (!urlParams) {
       return;
     }
     const page = pages.find(page => page.id === urlParams.page && !page.disabled);
