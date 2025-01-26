2d573b03052dafff10ee66d10dddef854a05f956
71walceli <71walceli@71walceli.dev>
2025-01-25 21:08:38 -0500

    Added missing types for transactions and storages

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,8 +1,8 @@
-import type { Transaction, UnsafeTransaction, StorageDescriptor } from "polkadot-api";
+import type { Transaction, UnsafeTransaction, StorageDescriptor, TxOptions } from "polkadot-api";
 
 export type ApiTx = UnsafeTransaction<unknown | any, string, string, any> 
   | Transaction<unknown | any, string, string, any>
 
 export type ApiStorage = StorageDescriptor<any, any, boolean, string> & {
-  getValue: (...args: unknown, options: { at?: "best" | "finalized" }) => Promise<any>
+  getValue: (...args: unknown, options?: TxOptions) => Promise<any>
 }
