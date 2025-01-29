6df9a85f852bc8c75dd12a77eb58bdba408298e9
71walceli <71walceli@71walceli.dev>
2025-01-29 16:45:06 -0500

    Add alt text to instruct users to send codes

--- src/components/tabs/ChallengePage.tsx
+++ src/components/tabs/ChallengePage.tsx
@@ -143,6 +143,12 @@ export function ChallengePage({
     discord: <DiscordIcon className="h-4 w-4" />,
     twitter: <XIcon className="h-4 w-4" />,
   }
+  const inviteInstreuctions = {
+    matrix: "Accept the invite and paste it in the Matrix chat",
+    email: "Send an emaiol to the provided address with the code",
+    discord: "Join the Discord server and paste the code in the #verification channel",
+    twitter: "Send a DM to the provided Twitter account with the code",
+  }
 
   return noChallenges > 0 
     ?<Card className="bg-transparent border-[#E6007A] text-inherit shadow-[0_0_10px_rgba(230,0,122,0.1)]">
@@ -188,7 +194,9 @@ export function ChallengePage({
                     </Button>
                     
                     {import.meta.env[`VITE_APP_INVITE_LINK_${field.toUpperCase()}`] && 
-                      <a href={import.meta.env[`VITE_APP_INVITE_LINK_${field.toUpperCase()}`]} target="_blank" rel="noreferrer">
+                      <a href={import.meta.env[`VITE_APP_INVITE_LINK_${field.toUpperCase()}`]} 
+                        target="_blank" rel="noreferrer" title={inviteInstreuctions[field]}
+                      >
                         <Button variant="outline" size="icon" 
                           className="border-[#E6007A] text-inherit hover:bg-[#E6007A] hover:text-[#FFFFFF] flex-shrink-0"
                         >
