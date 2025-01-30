5e17b60490efdc24be7c6120ac80033f3453ed92
71walceli <71walceli@71walceli.dev>
2025-01-30 16:45:06 -0500

    Add nix shelll script to set up build env

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [ curl unzip ];
+
+  /* LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+    openssl
+    sqlite
+    stdenv.cc.cc.lib
+  ]; */
+
+  shellHook = ''
+    set -euo pipefail
+    export BUN_INSTALL="$HOME/.bun"
+    export PATH="$BUN_INSTALL/bin:$PATH"
+    
+    if ! command -v bun &>/dev/null; then
+      curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.42"
+    fi
+
+    if [ ! -d node_modules ]; then
+      bun install
+    fi
+
+    # Wait for the descriptors to be downloaded. This is a workaround because it's likely to fail
+    # by not generating the descriptors on the first try.
+    attempts=10
+    while [ ! -d .papi/descriptors ]; do
+      bunx polkadot-api@1.8.0 update
+      attempts=$((attempts - 1))
+      if [ $attempts -eq 0 ]; then
+        echo "Failed to download descriptors"
+        exit 1
+      fi
+    done
+  '';
+}
