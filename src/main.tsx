76d6a4834351e7cd5c5ac148abf59396cc519fc2
71walceli <71walceli@71walceli.dev>
2025-02-05 13:44:41 -0500

    fix: enforce HTTPS protocol in production environment

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'uno.css'
@@ -8,6 +8,14 @@ import "./css/uno.css"
 import { Loading } from './pages/Loading'
 
 const Main: React.FC = () => {
+  useEffect(() => {
+    if (!import.meta.env.DEV) {
+      if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+      }
+    }
+  }, []);
+
   return <>
     <Suspense fallback={<Loading />}>
       <App />
