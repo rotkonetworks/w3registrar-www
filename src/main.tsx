dc003288af7d84bfa95c75ff3f5438c49c790897
71walceli <71walceli@71walceli.dev>
2025-02-09 00:45:10 -0500

    chore: import carousel styles from react-responsive-carousel

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import 'uno.css'
 import '@unocss/reset/tailwind-compat.css'
 import "./css/uno.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { Loading } from './pages/Loading'
 
