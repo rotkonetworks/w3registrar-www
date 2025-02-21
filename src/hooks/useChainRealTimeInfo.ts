d7de3a0aba20bdd1300ca8c4343355100683f6b0
71walceli <71walceli@71walceli.dev>
2025-02-20 22:58:47 -0500

    refactor: Remove previous pending blocks and event handling logic

--- src/hooks/useChainRealTimeInfo.ts
+++ src/hooks/useChainRealTimeInfo.ts
@@ -36,66 +36,7 @@ export const useChainRealTimeInfo = ({ typedApi, chainId, address, handlers }: {
       })()
     }
   }, [typedApi])
-
-  const _pendingBlocks = useRef([])
-  useEffect(() => {
-    const timer = window.setInterval(() => {
-      [..._pendingBlocks.current]
-        .sort((b1, b2) => 
-          b2.neta.block.number*100 + handlers[b2.type].priority - b1.neta.block.number*100 + handlers[b1.type].priority 
-        )
-        .forEach(block => {
-          handlers[block.type].onEvent(block)
-          if (import.meta.env.DEV) console.log({ block, })
-        })
-      ;
-      _pendingBlocks.current = []
-    },  CHAIN_UPDATE_INTERVAL)
-    return () => {
-      window.clearInterval(timer)
-      _pendingBlocks.current = []
-    }
-  }, [])
-
-  // Since a single account is used, we can keep track of the last block per relevant event type. 
-  // This way we can avoid processing the same event multiple times.
-  const _lastBlockPerEvent = useRef({})
-  const waitForEvent = ({ type: { pallet, call }, }) => {
-    const type = `${pallet}.${call}`;
-    const { onError } = handlers[type]
-    typedApi.event[pallet][call].pull()
-      .then(data => {
-        if (data.length === 0) {
-          return
-        }
-        if (import.meta.env.DEV) console.log({ data, type, address })
-        data.filter(item => 
-          [item.payload.who, item.payload.target].includes(address)
-            && item.meta.block.number > (_lastBlockPerEvent.current[type] || 0)
-        )
-          .forEach(item => {
-            _pendingBlocks.current.push({ ...item, type })
-            _lastBlockPerEvent.current[type] = item.meta.block.number
-          })
-      })
-      .catch(error => {
-        onError(error)
-        if (import.meta.env.DEV) console.error({ message: error.message, type, })
-        if (import.meta.env.DEV) console.error(error)
-      })
-  }
-  const getEffectCallback = ({ type: { pallet, call }, }) => {
-    return () => {
-      if (!chainId || !address) {
-        return
-      }
-      const timer = window.setInterval(() => {
-        waitForEvent({ type: { pallet, call }, })
-      }, CHAIN_UPDATE_INTERVAL)
-      return () => window.clearInterval(timer)
-    }
-  }
-
+  
   // Convert handlers to array and memoize
   const handlerEntries = useMemo(() => 
     Object.entries(handlers).map(([key, handler]) => {
@@ -104,7 +45,7 @@ export const useChainRealTimeInfo = ({ typedApi, chainId, address, handlers }: {
     }), 
     [handlers]
   )
-  
+
   useEffect(() => {
     const systemEventsSub = (typedApi.query.System.Events as ApiStorage)
       .watchValue("best").subscribe({
