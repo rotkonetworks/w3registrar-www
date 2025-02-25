f033998e10d1125028a27ed1f0b7544779f4f194
71walceli <71walceli@71walceli.dev>
2025-02-25 18:13:46 -0500

    feat: Simplify icon retrieval in ChallengePage by using SOCIAL_ICONS

--- src/components/tabs/ChallengePage.tsx
+++ src/components/tabs/ChallengePage.tsx
@@ -12,6 +12,7 @@ import { XIcon } from "~/assets/icons/x"
 import { DiscordIcon } from "~/assets/icons/discord"
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert"
 import { HelpCarousel } from "~/help/helpCarousel"
+import { SOCIAL_ICONS } from "~/assets/icons"
 
 export function ChallengePage({
   addNotification,
@@ -83,22 +84,7 @@ export function ChallengePage({
   }
 
   const getIcon = (field: string) => {
-    switch (field) {
-      case "matrix":
-        return <AtSign className="h-4 w-4" />
-      case "email":
-        return <Mail className="h-4 w-4" />
-      case "discord":
-        return <MessageSquare className="h-4 w-4" />
-      case "twitter":
-        return <MessageSquare className="h-4 w-4" />
-      case "web":
-        return <Globe className="h-4 w-4" />
-      case "display":
-        return <UserCircle className="h-4 w-4" />
-      default:
-        return null
-    }
+    return SOCIAL_ICONS[field]
   }
 
   const updateChallengeStatus = useCallback((field: keyof ChallengeStore, result: boolean) => {
