7b64c1e820701a423363c4e7465dc894ae2411af
71walceli <71walceli@71walceli.dev>
2025-04-26 19:44:08 -0500

    feat: Enhance TourProvider with custom arrow components and additional styles

--- src/main.tsx
+++ src/main.tsx
@@ -8,24 +8,47 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { Loading } from './pages/Loading'
 
-import { TourProvider } from '@reactour/tour'
+import { TourProvider, ArrowProps } from '@reactour/tour'
 import { MAIN_TOUR } from './help/Tours'
 import { useDarkMode } from './hooks/useDarkMode'
+import { Button } from './components/ui/button'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 
 const Main: React.FC = () => {
   const { isDark } = useDarkMode()
 
   return <>
     <Suspense fallback={<Loading />}>
-      <TourProvider steps={MAIN_TOUR} styles={{
-        popover: (style) => ({
-          ...style,
-          backgroundColor: !isDark 
-            ?"hsl(0 0% 100%)"
-            :"#2C2B2B"
-          ,
-        }),
-      }}>
+      <TourProvider steps={MAIN_TOUR} 
+        components={{
+          Arrow: ({ inverted, disabled }: ArrowProps) => (
+            <Button
+              variant='primary'
+              disabled={disabled}
+              className='rounded-full h-10 w-10'
+            >
+              {inverted ? <ArrowRight className="h-4 w-4" /> : <ArrowLeft className="h-4 w-4" />}
+            </Button>
+          ),
+        }}
+        styles={{
+          popover: (style) => ({
+            ...style,
+            backgroundColor: !isDark 
+              ?"hsl(0 0% 100%)"
+              :"#2C2B2B"
+            ,
+          }),
+          badge: (style) => ({
+            ...style,
+            backgroundColor: "rgb(230 0 122 / 1)",
+          }),
+          navigation: (style) => ({
+            ...style,
+            color: "rgb(230 0 122 / 1)",
+          }),
+        }}
+      >
         <App />
       </TourProvider>
     </Suspense>
