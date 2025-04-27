f4960680ddef9eb6dbc5be00defc65253908e673
71walceli <71walceli@71walceli.dev>
2025-04-26 20:38:46 -0500

    feat: Update MAIN_TOUR steps to enhance guidance on account selection, network management, and balance display

--- src/help/Tours.tsx
+++ src/help/Tours.tsx
@@ -5,7 +5,8 @@ export const MAIN_TOUR: StepType[] = [
     selector: ".AccountSelect",
     content: <div>
       <p className="text-sm">
-        Select an account to use with this identity. You can also create a new account.
+        Select an account to use to manage identity. You can also manage connections to wallets.
+        Balance is shown here for every connected account.
       </p>
     </div>,
   }, 
@@ -13,7 +14,24 @@ export const MAIN_TOUR: StepType[] = [
     selector: ".NetworkSelect",
     content: <div>
       <p className="text-sm">
-        Select a network to use with this identity. You can also create a new network.
+        Select a network in which to manage your identity.
+      </p>
+    </div>,
+  },
+  {
+    selector: ".MainBalanceDisplay",
+    content: <div>
+      <p className="text-sm">
+        This is your main balance for chosen account on this network. Balance will always be shown in transaction 
+        dialogs to help, for your convenience.
+      </p>
+    </div>,
+  },
+  {
+    selector: ".HeaderActions",
+    content: <div>
+      <p className="text-sm">
+        These are various controls for the app, that let you display help dialog and toggle dark mode.
       </p>
     </div>,
   }
