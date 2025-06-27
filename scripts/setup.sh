b8f7fba7a18c085105ef3dc74112fb7039c5c281
71walceli <71walceli@71walceli.dev>
2025-06-27 06:48:33 -0500

    fix: Simplify Polkadot API installation in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,8 +9,8 @@ fi
 # by not generating the descriptors on the first try.
 attempts=10
 while [ ! -d .papi/descriptors/dist ]; do
-    polkadot_api_version=$(jq -r '.dependencies["polkadot-api"]' package.json | sed 's/[^0-9.]*//g')
-    bunx polkadot-api@$polkadot_api_version update || true
+    bun install polkadot-api@latest
+    bunx polkadot-api@latest update || true
     attempts=$((attempts - 1))
     if [ $attempts -eq 0 ]; then
         echo "Failed to download descriptors"
