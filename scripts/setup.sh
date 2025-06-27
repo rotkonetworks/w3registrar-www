371eff19a53e3f9c52891ee0b68d70ec1cc29dfe
71walceli <71walceli@71walceli.dev>
2025-06-26 21:06:57 -0500

    fix: Update Polkadot API version retrieval in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,8 @@ fi
 # by not generating the descriptors on the first try.
 attempts=10
 while [ ! -d .papi/descriptors/dist ]; do
-    bunx polkadot-api@1.8.0 update || true
+    polkadot_api_version=$(jq -r '.dependencies["polkadot-api"]' package.json | sed 's/[^0-9.]*//g')
+    bunx polkadot-api@$polkadot_api_version update || true
     attempts=$((attempts - 1))
     if [ $attempts -eq 0 ]; then
         echo "Failed to download descriptors"
